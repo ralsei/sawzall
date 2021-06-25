5eca5a00d508dedd268163c676dcaa33b8ee8011
hazel levine <hazel@knightsofthelambdacalcul.us>
2021-06-24 20:18:56 -0400

    plotting with wrangled data

---
+++ examples/5.2.rkt
@@ -0,0 +1,39 @@
+#lang racket
+(require data-frame
+         graphite
+         threading
+         racket/vector
+         "../main.rkt") ; sawzall
+
+(define (v/ vec c) (vector-map (λ (v) (/ v c)) vec))
+(define (sum vec) (for/sum ([v (in-vector vec)] #:when (number? v)) v))
+
+(define gss-sm (df-read/csv "data/gss_sm.csv"))
+;; rel_by_region <- gss_sm %>%
+;;     group_by(bigregion, religion) %>%
+;;     summarize(N = n()) %>%
+;;     mutate(freq = N / sum(N),
+;;            pct = round((freq*100), 0))
+(define rel-by-region
+  (~> gss-sm
+      (aggregate '("bigregion" "religion")
+                 (saw-λ [N (religion) (vector-length religion)]))
+      (facet "bigregion")
+      (map (lambda~> (create-all (saw-λ [freq (N) (v/ N (sum N))]))
+                     (create (saw-λ [pct (freq) (round (* freq 100))])))
+           _)
+      (apply unfacet _)))
+
+;; p <- ggplot(rel_by_region, aes(x = religion, y = pct, fill = religion))
+;; p + geom_col(position = "dodge2") +
+;;     labs(x = NULL, y = "Percent", fill = "Religion") +
+;;     guides(fill = FALSE) +
+;;     coord_flip() +
+;;     facet_grid(~ bigregion)
+(graph #:data rel-by-region
+       #:mapping (aes #:x "religion" #:y "pct" #:facet "bigregion")
+       #:facet-wrap 2
+       #:x-label "Religion" #:y-label "Percent"
+       #:height 800
+       #:width 800
+       (col))
--- main.rkt
+++ main.rkt
@@ -1,56 +1,11 @@
 #lang racket/base
-(require data-frame
-         plot/utils
-         threading
-         "aggregate.rkt"
+(require "aggregate.rkt"
          "create.rkt"
          "display.rkt"
          "facet.rkt"
          "saw-lambda.rkt")
-
-(define (sum vec)
-  (for/sum ([v (in-vector vec)] #:when (number? v)) v))
-
-; entry-point
-(define gss-sm (df-read/csv "data/gss_sm.csv"))
-; group by bigregion and religion, make a new column total that is
-; the total of each religion in each region, _remove other columns_
-(define aggregated
-  (aggregate gss-sm
-             '("bigregion" "religion")
-             (saw-λ [total (religion) (vector-length religion)]))) ; argument is a vector here
-; split along religion
-(define faceted (facet aggregated "bigregion"))
-; for each split, make a new table with the frequency of each religion's occurrence
-; in that region
-(define with-frequencies
-  (map (λ (df)
-         (create df (saw-λ [frequency (total) (/ total (sum (df-select df "total")))]
-                           [percent (frequency) (round (* frequency 100))])))
-       faceted))
-; then combine them back together
-(define with-frequency (apply unfacet with-frequencies))
-; and let 'er rip
-(show with-frequency)
-
-; alternatively
-(~> gss-sm
-    (aggregate '("bigregion" "religion")
-               (saw-λ [total (religion) (vector-length religion)]))
-    (facet "bigregion")
-    (map (λ (df)
-           (~> df
-               (create-all (saw-λ [frequency (total) (v/ total (sum total))]))
-               (create (saw-λ [percent (frequency) (round (* frequency 100))]))))
-         _)
-    (apply unfacet _)
-    show)
-
-; alternatively alternatively??
-;; (~> gss-sm
-;;     (group-with "bigregion" "religion") ; this turns into a data-frame + grouping info
-;;     (aggregate (saw-λ [total (religion) (vector-length religion)]))
-;;     (create-all (saw-λ [frequency (total) (v/ total (sum total))]))
-;;     (create (saw-λ [percent (frequency) (round (* frequency 100))]))
-;;     ungroup ; turns back into a data-frame
-;;     show)
+(provide facet unfacet
+         aggregate
+         create create-all
+         show introspect
+         saw-λ saw-proc?)
