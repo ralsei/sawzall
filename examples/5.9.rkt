151922a81fdd7f8b992346ec6593581f4b0ae0ad
hazel levine <hazel@knightsofthelambdacalcul.us>
2021-06-30 17:25:19 -0400

    what the fuck is a free monoid sarah

--- examples/5.9.rkt
+++ examples/5.9.rkt
@@ -1,7 +1,18 @@
 #lang racket
-(require data-frame graphite "../main.rkt")
+(require data-frame math/statistics graphite "../main.rkt")
 
-(define organdata (df-read/csv "data/organdata.csv"))
+(define organdata (df-read/csv "data/organdata.csv" #:na "NA"))
 (define sorted
   (reorder organdata
-           [country string-ci<?]))
+           [country (λ (a b)
+                      (for/fold ([as null] [bs null] #:result (< (mean as) (mean bs)))
+                                ([(val to-mean) (in-data-frame organdata "country" "donors")]
+                                 #:when to-mean)
+                        (cond [(equal? val a) (values (cons to-mean as) bs)]
+                              [(equal? val b) (values as (cons to-mean bs))]
+                              [else (values as bs)])))]))
+
+(graph #:data sorted
+       #:mapping (aes #:x "donors" #:y "country")
+       #:width 700
+       (boxplot #:invert? #t))
