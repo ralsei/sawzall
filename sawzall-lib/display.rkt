94cb15f4f3fc41ff9c1e6dff241941c7117a53c5
hazel levine <hazel@knightsofthelambdacalcul.us>
2021-07-01 14:44:41 -0400

    minor display stuff

--- sawzall-lib/display.rkt
+++ sawzall-lib/display.rkt
@@ -1,20 +1,39 @@
 #lang racket/base
 (require data-frame
+         fancy-app
          text-table
+         racket/list
          "grouping.rkt")
 (provide show introspect)
 
-(define (show df)
-  (void (group-map show-internal df #:pass-groups? #t)))
+(define *show-rows-default* 6)
+(define *show-cols-default* 6)
 
-(define (show-internal df grps)
+(define (show df #:all? [all? #f])
+  (void (group-map (show-internal _ _ all?) df #:pass-groups? #t)))
+
+(define (show-internal df grps all?)
+  (define all-series (df-series-names df))
+  (define n-rows (df-row-count df))
+  (define n-cols (length all-series))
+  (define row-cap (min *show-rows-default* n-rows))
+  (define col-cap (min *show-cols-default* n-cols))
+
+  (printf "data-frame: ~a rows x ~a columns~n" n-rows n-cols)
   (when (not (null? grps))
     (printf "groups: ~a~n" grps))
+
   (print-table
-   (let ([series (df-series-names df)])
+   (let ([series (take all-series col-cap)])
      (cons series
-           (for/list ([v (apply in-data-frame/list df series)])
-             v)))))
+           (for/list ([v (apply in-data-frame/list df series)]
+                      [_ row-cap])
+             v))))
+
+  (when (not all?)
+    (printf "~a rows, ~a cols elided (use #:all? for full frame)~n"
+            (- n-rows row-cap)
+            (- n-cols col-cap))))
 
 (define (introspect df)
   (show df) df)
