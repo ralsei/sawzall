375113384b70548dfa03441ae06b9d47f4f32af6
hazel levine <hazel@knightsofthelambdacalcul.us>
2021-07-19 15:48:17 -0400

    that shouldn't be commented out

--- sawzall-examples/r4ds-ch5.rkt
+++ sawzall-examples/r4ds-ch5.rkt
@@ -31,28 +31,28 @@
                  [delay (arr_delay) (mean (vector-filter identity arr_delay))])
       (where (count dest) (and (> count 20) (not (equal? dest "HNL"))))))
 
-;; (graph #:data delays
-;;        #:mapping (aes #:x "dist" #:y "delay")
-;;        #:theme (theme-override theme-default #:color-map 'tol-sd)
-;;        (points #:mapping (aes #:continuous-color "count"))
-;;        (fit #:method 'loess #:width 3))
+(graph #:data delays
+       #:mapping (aes #:x "dist" #:y "delay")
+       #:theme (theme-override theme-default #:color-map 'tol-sd)
+       (points #:mapping (aes #:continuous-color "count"))
+       (fit #:method 'loess #:width 3))
 
-;; ;; get all the flights that haven't been cancelled (which is a NA delay)
-;; (define not-cancelled
-;;   (~> flights
-;;       (where (arr_delay dep_delay) (and arr_delay dep_delay))))
+;; get all the flights that haven't been cancelled (which is a NA delay)
+(define not-cancelled
+  (~> flights
+      (where (arr_delay dep_delay) (and arr_delay dep_delay))))
 
-;; ;; planes identified by tail number with the highest average delays
-;; ;; filter out groups with small numbers of observations, to get to more trends
-;; (define delays-by-tailnum
-;;   (~> not-cancelled
-;;       (group-with "tailnum")
-;;       (aggregate [delay (arr_delay) (mean arr_delay)]
-;;                  [N (arr_delay) (vector-length arr_delay)])
-;;       (where (N) (> N 25))))
+;; planes identified by tail number with the highest average delays
+;; filter out groups with small numbers of observations, to get to more trends
+(define delays-by-tailnum
+  (~> not-cancelled
+      (group-with "tailnum")
+      (aggregate [delay (arr_delay) (mean arr_delay)]
+                 [N (arr_delay) (vector-length arr_delay)])
+      (where (N) (> N 25))))
 
-;; ;; we don't have geom_freqpoly() in graphite. maybe we should?
-;; (graph #:data delays-by-tailnum
-;;        #:mapping (aes #:x "N" #:y "delay")
-;;        #:x-min -2 #:width 700
-;;        (points #:alpha 1/10 #:color "black"))
+;; we don't have geom_freqpoly() in graphite. maybe we should?
+(graph #:data delays-by-tailnum
+       #:mapping (aes #:x "N" #:y "delay")
+       #:x-min -2 #:width 700
+       (points #:alpha 1/10 #:color "black"))
